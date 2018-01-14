9af1a430ca3fb2e312b5e4d06d9642d84b058939
KishManani <kishan_manani@hotmail.com>
2018-01-14 22:26:11 +0000

    edited unit test

--- tests/unit_tests/test_initialisation.py
+++ tests/unit_tests/test_initialisation.py
@@ -32,9 +32,15 @@ def test_connection_matrix_initialisation(heart):
     # Setup
     h = heart
     # Exercise
-    result = h.connections[:,:,0].sum() / (h.col_size * h.row_size)
-    expected = h.prob_con
+    result = h.connections[:,:,:].sum(axis=(0,1), keepdims=True).squeeze() / (h.col_size * h.row_size)
+    expected = np.zeros((4,1))
+    expected[0] = h.prob_con # north/up neighbour
+    expected[2] = h.prob_con # south/down neighbour
+    expected[1] = 1 # east/right neighbour
+    expected[3] = 1 # west/left neighbour
     # Validate
-    assert_approx_equal(result, expected, significant=1)
-
+    assert_approx_equal(result[0], expected[0], significant=1)
+    assert_approx_equal(result[1], expected[1], significant=1)
+    assert_approx_equal(result[2], expected[2], significant=1)
+    assert_approx_equal(result[3], expected[3], significant=1)
 
