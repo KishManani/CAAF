325b2b446d4b0707f3cf55558f5608cc33a3f604
KishManani <kishan_manani@hotmail.com>
2018-01-14 20:36:39 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
-    name='CAAF',  # Required
-    version='0.0.1',  # Required
-    description='A Python implementation of a cellular automaton model of atrial fibrillation',  # Required
+    name='CAAF',
+    version='0.0.1',
+    description='A Python implementation of a cellular automaton model of atrial fibrillation',
     url='https://github.com/KishManani/CAAF',
     author='Kishan Manani',
     classifiers=[
         'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     install_requires=['numpy', 'matplotlib', 'numba']
